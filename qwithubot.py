21a49f756a8af8f0d2b0b855a67d8b4e050d8cee
zeka2007 <75169925+zeka2007@users.noreply.github.com>
2021-01-18 13:56:27 +0300

    Update qwithubot.py

--- qwithubot.py
+++ qwithubot.py
@@ -399,7 +399,8 @@ async def help(ctx):
     emb.add_field(name = 'Настройка:', value = 'playMusic(on/off), removeJoinChannel, setJoinChannel(канал)')
     await ctx.send(embed = emb)
 @client.event
-async def on_voise_state_update(member, before, after):
-    await before.channel.disconect()
+async def on_voice_state_update(member, before, after):
+    channel = await before.channel.connect()
+    await channel.disconnect()
 token = os.environ.get('BOT_TOKEN')
 client.run(token)
